d993ad3ea05c05e818e5419f147ace68a855c1cc
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-05-30 17:24:11 +0300

    corrected errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Statistics from './Statistics/Statistics';
-import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
-import Section from './Section/Section';
-import Notification from './Notification/Notification';
+//import Statistics from './Statistics/Statistics';
+//import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+//import Section from './Section/Section';
+//import Notification from './Notification/Notification';
 
 class App extends Component {
   state = {
@@ -11,13 +11,13 @@ class App extends Component {
     bad: 0,
   };
 
-  countTotalFeedback();
+  //countTotalFeedback();
 
-  countPositiveFeedbackPercentage();
+  //countPositiveFeedbackPercentage();
 
   render() {
-    return (``)
+    return ``;
   }
 }
-   
-  
+
+export default App;
